f80cd673dcc455aa9493dbac86b06a11ab9736d1
Headdawn Light <sertanix@yandex.com>
2022-09-24 20:55:01 +0300

    Initial project files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import AppHeader from "./AppHeader";
+import "../styles/css/App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <main>
+        <AppHeader />
+      </main>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/AppHeader.js
@@ -0,0 +1,12 @@
+const AppHeader = () => {
+  return (
+    <header className="App-header">
+      <h1>
+        <span className="App-title">sociality</span>
+        <span className="App-title-ex">.io</span>
+      </h1>
+    </header>
+  );
+};
+
+export default AppHeader;
