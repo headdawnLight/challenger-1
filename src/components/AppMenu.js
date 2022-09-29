0ffb3acc29e9f02c84e6f3b508f60982169e08f8
Headdawn Light <sertanix@yandex.com>
2022-09-30 00:16:15 +0300

    Create accordion menu

--- src/components/AppMenu.js
+++ src/components/AppMenu.js
@@ -1,43 +1,57 @@
+import { useState } from "react";
+
 const AppMenu = () => {
+  const [selected, setSelected] = useState(null);
+
+  const menuList = [
+    {
+      title: "Menu Zero",
+      text: "Lorem ipsum",
+    },
+    {
+      title: "Menu One",
+      text: "Lorem ipsum",
+    },
+    {
+      title: "Menu Two",
+      text: "Lorem ipsum",
+    },
+    {
+      title: "Menu Three",
+      text: "Lorem ipsum",
+    },
+    {
+      title: "Menu Four",
+      text: "Lorem ipsum",
+    },
+    {
+      title: "Menu Five",
+      text: "Lorem ipsum",
+    },
+  ];
+
+  const toggle = (i) => {
+    if (selected === i) {
+      return setSelected(null);
+    }
+
+    setSelected(i);
+  };
   return (
     <section className="App-menu">
       <div className="Accordion-menu">
-        <div className="Content-box">
-          <div className="Content-title">Menu Zero</div>
-          <div className="Content-text">
-            <p>Lorem ipsum</p>
+        {menuList.map((item, i) => (
+          <div
+            className={selected === i ? "Content-box active" : "Content-box"}
+          >
+            <div className="Content-title" onClick={() => toggle(i)}>
+              <h2>{item.title}</h2>
+            </div>
+            <div className="Content-text">
+              <p>{item.text}</p>
+            </div>
           </div>
-        </div>
-        <div className="Content-box">
-          <div className="Content-title">Menu One</div>
-          <div className="Content-text">
-            <p>Lorem ipsum</p>
-          </div>
-        </div>
-        <div className="Content-box">
-          <div className="Content-title">Menu Two</div>
-          <div className="Content-text">
-            <p>Lorem ipsum</p>
-          </div>
-        </div>
-        <div className="Content-box">
-          <div className="Content-title">Menu Three</div>
-          <div className="Content-text">
-            <p>Lorem ipsum</p>
-          </div>
-        </div>
-        <div className="Content-box">
-          <div className="Content-title">Menu Four</div>
-          <div className="Content-text">
-            <p>Lorem ipsum</p>
-          </div>
-        </div>
-        <div className="Content-box">
-          <div className="Content-title">Menu Five</div>
-          <div className="Content-text">
-            <p>Lorem ipsum</p>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
